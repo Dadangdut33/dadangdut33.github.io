6fa0a34ad09f024f0cc5a94384f4b707d97f43ef
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2021-12-18 15:49:15 +0700

    fix deploy, gonna test again

--- src/Components/MainPage/BottomTest.jsx
+++ src/Components/MainPage/BottomTest.jsx
@@ -2,7 +2,7 @@ import Footer from "../Footer/Footer";
 
 export default function BottomTest() {
 	return (
-		<section className='flex-column'>
+		<section className='flex-dir-column'>
 			<div className='center-horizontal-vertical'>
 				<h1>aaaaaa</h1>
 			</div>
