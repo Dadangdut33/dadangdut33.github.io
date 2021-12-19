8f24402ec52fe10a1d6161a26a726b592a98c6f4
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2021-12-19 23:23:43 +0700

    about

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-// import $ from "jquery";
-// import Popper from "popper.js";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
--- src/Components/MainPage/About.jsx
+++ src/Components/MainPage/About.jsx
@@ -1,8 +1,21 @@
+import ReactTooltip from "react-tooltip";
 export default function MainPage() {
 	return (
 		<section className='center-flex-column' id='about-me'>
-			<div className='center-horizontal-vertical'>
-				<h1>A little more about me</h1>
+			<div className='center-horizontal-vertical justify wrap-mid'>
+				<h1 className='display-1'>A little more about me</h1>
+				<h5 className='text-muted'>
+					At first, I have never thought of becoming a programmer because I had a feeling that it's a{" "}
+					<strong data-tip data-for='imgPopup-1'>
+						hard job
+					</strong>
+					<ReactTooltip id='imgPopup-1' className='tooltip-img'>
+						<img src='https://c.tenor.com/OB4K11sbmrMAAAAM/spongebob-thinking.gif' />
+					</ReactTooltip>
+					, but then i realize that other stuff are also hard so why don't i try to pursue something that i could enjoy and so... here I am.
+				</h5>
+				<h5 className='text-muted'>I like games, anime, movie, manga, etc. I am very interested in programming, especially in topics related to stuff that I like.</h5>
+				<h5 className='text-muted'>I have a passion for learning new things and I am always trying to improve my skills.</h5>
 			</div>
 		</section>
 	);
