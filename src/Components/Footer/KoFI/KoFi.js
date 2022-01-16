5d79a08150899237d6cd337c5070b5abb16b48e9
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-01-16 14:52:24 +0700

    center the btn

--- src/Components/Footer/KoFI/KoFi.js
+++ src/Components/Footer/KoFI/KoFi.js
@@ -5,7 +5,7 @@ import "./KoFi.css";
 export default function KoFi(props) {
 	const { color, id, label } = props;
 	return (
-		<div class='btn-container'>
+		<div>
 			<a title={label} class='kofi-button' style={{ backgroundColor: color }} href={"https://ko-fi.com/" + id} target='_blank' rel='noopener noreferrer'>
 				<span class='kofitext'>
 					<img src='https://ko-fi.com/img/cup-border.png' class='kofiimg' alt='Ko-Fi button' />
