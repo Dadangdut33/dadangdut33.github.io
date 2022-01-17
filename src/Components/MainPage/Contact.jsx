6ebe3d7ed08a357bc9944002825d8f603aee0f39
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-01-18 00:05:41 +0700

    arrow hover on contact

--- src/Components/MainPage/Contact.jsx
+++ src/Components/MainPage/Contact.jsx
@@ -1,6 +1,7 @@
 import Footer from "../Footer/Footer";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
+import { useState } from "react";
 export default function BottomTest() {
 	const [contentRef, contentInView] = useInView({
 		triggerOnce: false,
@@ -22,6 +23,25 @@ export default function BottomTest() {
 		},
 	};
 
+	const [hoverLink_1, setHoverLink_1] = useState(false);
+	const [hoverLink_2, setHoverLink_2] = useState(false);
+	const [hoverLink_3, setHoverLink_3] = useState(false);
+
+	const linkFadeVariants = {
+		hidden: {
+			opacity: 0,
+			transition: {
+				duration: 0.3,
+			},
+		},
+		visible: {
+			opacity: 1,
+			transition: {
+				duration: 0.3,
+			},
+		},
+	};
+
 	return (
 		<section className='flex-dir-column' id='contact-me'>
 			<div className='center-horizontal-vertical'>
@@ -29,18 +49,37 @@ export default function BottomTest() {
 				<motion.div ref={contentRef} initial='hidden' animate={contentInView ? "visible" : "hidden"} variants={contentVariants} style={{ overflow: "hidden" }}>
 					<div className='center-horizontal-vertical justify wrap-mid'>
 						<p style={{ minWidth: "210px" }}>
-							<i class='bi bi-envelope'></i> <a href='mailto:dadang.contact@gmail.com'>dadang.contact@gmail.com</a>
+							<i class='bi bi-envelope'></i>{" "}
+							<a href='mailto:dadang.contact@gmail.com' target={"_blank"} rel='noreferrer' className='no-decoration' onMouseEnter={() => setHoverLink_1(true)} onMouseLeave={() => setHoverLink_1(false)}>
+								dadang.contact@gmail.com{" "}
+								<motion.span variants={linkFadeVariants} initial='hidden' animate={hoverLink_1 ? "visible" : "hidden"}>
+									◀
+								</motion.span>
+							</a>
 						</p>
 						<p>
 							<i className='bi bi-github'></i>{" "}
-							<a href='https://github.com/Dadangdut33' target={"_blank"} rel='noreferrer'>
-								Dadangdut33
+							<a href='https://github.com/Dadangdut33' target={"_blank"} rel='noreferrer' className='no-decoration' onMouseEnter={() => setHoverLink_2(true)} onMouseLeave={() => setHoverLink_2(false)}>
+								Dadangdut33{" "}
+								<motion.span variants={linkFadeVariants} initial='hidden' animate={hoverLink_2 ? "visible" : "hidden"}>
+									◀
+								</motion.span>
 							</a>
 						</p>
 						<p>
 							<i className='bi bi-linkedin'></i>{" "}
-							<a href='https://www.linkedin.com/in/fauzan-farhan-antoro/' target={"_blank"} rel='noreferrer'>
-								Fauzan Farhan Antoro
+							<a
+								href='https://www.linkedin.com/in/fauzan-farhan-antoro/'
+								target={"_blank"}
+								rel='noreferrer'
+								className='no-decoration'
+								onMouseEnter={() => setHoverLink_3(true)}
+								onMouseLeave={() => setHoverLink_3(false)}
+							>
+								Fauzan Farhan Antoro{" "}
+								<motion.span variants={linkFadeVariants} initial='hidden' animate={hoverLink_3 ? "visible" : "hidden"}>
+									◀
+								</motion.span>
 							</a>
 						</p>
 					</div>
