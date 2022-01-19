ad5c7d765c68bf814202f0f52f72dcd277628ed5
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-01-19 19:26:08 +0700

    subtle link hover

--- src/Components/MainPage/About.jsx
+++ src/Components/MainPage/About.jsx
@@ -1,10 +1,16 @@
 import ReactTooltip from "react-tooltip";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
+
 export default function MainPage() {
 	const [contentRef, contentInView] = useInView({
 		triggerOnce: true,
 		rootMargin: "-250px 0px",
+	});
+
+	const [contentRef2, contentInView2] = useInView({
+		triggerOnce: false,
+		rootMargin: "-300px 0px",
 	});
 
 	const contentVariants = {
@@ -32,18 +38,20 @@ export default function MainPage() {
 					A little more about me
 				</h1>
 				<motion.div className='about-me content-outline' ref={contentRef} initial='hidden' animate={contentInView ? "visible" : "hidden"} variants={contentVariants}>
-					<p className='subtle-text'>
-						At first, I have never thought of becoming a programmer because I had a feeling that it's a{" "}
-						<strong data-tip data-for='imgPopup-1'>
-							hard job
-						</strong>
-						, but then i realize that other stuff are also hard. So, why don't i try to pursue something that i could enjoy and ... here I am.
-					</p>
-					<p className='subtle-text'>
-						I like games, anime, movie, manga, etc. I am very interested in programming, especially in topics related to stuff that I like. I have a passion for learning new things and I am always trying to
-						improve my skills.
-					</p>
-					<p className='subtle-text'>I really hope things that i create can be useful, even if just a little.</p>
+					<div ref={contentRef2}>
+						<p className='subtle-text'>
+							At first, I have never thought of becoming a programmer because I had a feeling that it's a{" "}
+							<strong data-tip data-for='imgPopup-1' className='subtle-link'>
+								hard job
+							</strong>
+							, but then i realize that other stuff are also hard. So, why don't i try to pursue something that i could enjoy and ... here I am.
+						</p>
+						<p className='subtle-text'>
+							I like games, anime, movie, manga, etc. I am very interested in programming, especially in topics related to stuff that I like. I have a passion for learning new things and I am always trying
+							to improve my skills.
+						</p>
+						<p className='subtle-text'>I really hope things that i create can be useful, even if just a little.</p>
+					</div>
 				</motion.div>
 				<ReactTooltip id='imgPopup-1' className='tooltip-img'>
 					<img src='https://c.tenor.com/OB4K11sbmrMAAAAM/spongebob-thinking.gif' alt='thinking-hard-gif' />
